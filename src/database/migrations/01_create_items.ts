cc5d395961f5644849be28d9347e10ab367a20ed
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-09-30 23:56:52 -0300

    items seed and image items

--- src/database/migrations/01_create_items.ts
+++ src/database/migrations/01_create_items.ts
@@ -6,8 +6,8 @@ export async function up(knex: Knex) {
     table.string('image').notNullable();
     table.string('title').notNullable();
   });
-};
+}
 
 export async function down(knex: Knex) {
   return knex.schema.dropTable('items');
-};
+}
